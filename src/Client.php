4cebb3fbbf74381839a6bc958e6fb794b6923fe3
gabriel.souza@grupoadolfolutz.com.br <gmdoas.dev@gmail.com>
2025-03-03 15:43:23 -0300

    ..

--- src/Client.php
+++ src/Client.php
@@ -1,3 +1,29 @@
 <?php
 
+class Client
+{
+    private $certificado;
+    private $homologacao;
 
+    public function __construct(array $config)
+    {
+        $this->certificado = isset($config['certificado']) ? $config['certificado'] : '';
+        $this->homologacao = isset($config['homologacao']) ? (bool) $config['homologacao'] : true;
+        
+        $this->inicializar();
+    }
+
+    private function inicializar()
+    {
+        if (!file_exists($this->certificado)) {
+            throw new \Exception("Certificado não encontrado: {$this->certificado}");
+        }
+
+        // Aqui você pode carregar o certificado, criar uma conexão segura, etc.
+    }
+
+    public function getAmbiente()
+    {
+        return $this->homologacao ? 'Homologação' : 'Produção';
+    }
+}
